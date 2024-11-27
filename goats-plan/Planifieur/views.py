62a7698d1d184e1e2f09927b36d404aca1516cb1
CBerl139 <berlioz.clement.28@gmail.com>
2024-11-27 04:15:11 +0100

    user now cannot view planning if not logged in (buggy)

--- goats-plan/Planifieur/views.py
+++ goats-plan/Planifieur/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Tache
 
 
-class Planning(ListView):
+class Planning(LoginRequiredMixin, ListView):
     model = Tache
     context_object_name = 'Tache'
     template_name = "Planifieur/index.html"
--- goats-plan/users/urls.py
+++ goats-plan/users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='users/login.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='users:login'), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
 ]
