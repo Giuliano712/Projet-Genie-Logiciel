718263515487389042ed88e47fef6bb3e7f0e44d
Clément Hiblot-Méry <c0hiblot@enib.fr>
2024-12-05 20:15:56 -0500

    pop up task

--- goats-plan/planner/models.py
+++ goats-plan/planner/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User  # Import du modèle utilisateur
+
 
 # Create your models here.
 class Tache(models.Model):
@@ -8,19 +10,25 @@ class Tache(models.Model):
         ('Haute', 'Haute'),
     ]
     ETAT_CHOICES = [
-        ('TO DO','TO DO'),
-        ('DOING','DOING'),
-        ('DONE','DONE'),
+        ('TO DO', 'TO DO'),
+        ('DOING', 'DOING'),
+        ('DONE', 'DONE'),
     ]
+
     titre = models.CharField(max_length=100)
     etat = models.CharField(
         max_length=100,
         choices=ETAT_CHOICES,
-        default='TO DO')
-
+        default='TO DO'
+    )
     importance = models.CharField(
         max_length=100,
         choices=IMPORTANCE_CHOICES,
         default='Moyenne'
     )
     deadline = models.CharField(max_length=100)
+    commentaire = models.TextField(blank=True, null=True)  # Champ pour les commentaires
+    utilisateurs = models.ManyToManyField(User, related_name="taches")  # Relation avec les utilisateurs
+
+    def __str__(self):
+        return self.titre
--- goats-plan/planner/views.py
+++ goats-plan/planner/views.py
@@ -2,9 +2,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Tache
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+
 
 
 class Planner(LoginRequiredMixin, ListView):
     model = Tache
     context_object_name = 'Tache'
     template_name = "planner/index.html"
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Tache
+    context_object_name = 'task'  # Utilisez un nom pour accéder aux données dans le template
+    template_name = "planner/task_detail.html"  # Chemin vers votre template HTML
+
+
